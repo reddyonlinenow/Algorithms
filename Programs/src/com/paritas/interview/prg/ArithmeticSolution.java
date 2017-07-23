2422495f0ba593c6fe83d3e5ffe1bd5435263685
Rajeswara Reddy <rvalishekkagari@walmartlabs.com>
2017-07-23 14:34:05 -0700

    added new programs

---
+++ Programs/src/com/paritas/interview/prg/ArithmeticSolution.java
@@ -0,0 +1,27 @@
+package com.paritas.interview.prg;
+
+import java.util.*;
+import java.math.*;
+
+public class ArithmeticSolution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double tip = mealCost * ((double)tipPercent / 100);
+        System.out.println(tip);
+        double tax = mealCost * ((double)taxPercent / 100);
+        double total = mealCost + tip + tax;
+      
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(total/*numberToRoundHere*/);
+      
+        System.out.println("The total meal cost is "+ totalCost +" dollars.");
+        // Print your result
+    }
+}
